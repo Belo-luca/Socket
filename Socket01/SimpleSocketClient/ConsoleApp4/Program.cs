ca8ee3c55690ec3dd7c9d36df4f8be332d9eac67
Luca_belometti <Belometti.luca.studente@itispaleocapa.it>
2026-09-22 12:43:02 +0200

    Socket 1

---
+++ Socket01/SimpleSocketClient/ConsoleApp4/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketClient
+{
+
+    public static void StartClient()
+    {
+        // Data buffer for incoming data.  
+        byte[] bytes = new byte[1024];
+
+        // Connect to a remote device.  
+        try
+        {
+            // Establish the remote endpoint for the socket.  
+            // This example uses port 11000 on the local computer.  
+            IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5000);
+
+            // Create a TCP/IP  socket.  
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint. Catch any errors.  
+            try
+            {
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}",
+                    sender.RemoteEndPoint.ToString());
+
+                // Encode the data string into a byte array.  
+                byte[] messaggio = Encoding.ASCII.GetBytes("This is a test,");
+                byte[] msg2 = Encoding.ASCII.GetBytes(" Ciao sono luca$");
+                byte[] msg = new byte[messaggio.Length + msg2.Length];
+
+                Buffer.BlockCopy(messaggio, 0, msg, 0, messaggio.Length);
+                Buffer.BlockCopy(msg2, 0, msg, messaggio.Length, msg2.Length);
+                // Send the data through the socket.
+                int bytesSent = sender.Send(msg);
+
+                string data = "";
+                do
+                {
+                    int bytesRec = sender.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                } while (data.IndexOf("$") == -1);
+                Console.WriteLine("Echoed test = {0}",
+                    data);
+
+                // Release the socket.  
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public static int Main(String[] args)
+    {
+        StartClient();
+        Console.WriteLine("Premere un tasto per continuare ");
+        Console.ReadLine();
+        return 0;
+    }
+}
